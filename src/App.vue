86e10965018e77e229d20941a976dcf99f7abfb9
bangnokia <bangnokia@gmail.com>
2024-02-12 22:26:30 +0700

    update tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+const pendingImage = 'https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif';
+const yesImage = 'https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="w-dvh h-dvh grid place-items-center">
+    <div class="flex flex-col">
+      <img :src="pendingImage" class="h-[200px]" alt="pending" />
+    </div>
+  </main>
+  <h1>this is the main page</h1>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
